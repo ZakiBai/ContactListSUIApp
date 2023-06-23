bc92598b98b5394ee92615bdb5d1dc333fd9a942
Zaki <menza.zm@gmail.com>
2023-06-23 17:24:10 +0600

    Initial Commit

---
+++ ContactListSUIApp/ContactListSUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIAppApp.swift
+//  ContactListSUIApp
+//
+//  Created by Zaki on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
